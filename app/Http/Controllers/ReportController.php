ff7d8b099017c973c61a559c671d5ca452f2d134
hiromitsuito <pfjqm042@yahoo.co.jp>
2020-09-13 19:49:02 +0900

    エラー修正
    エラー修正

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -15,11 +15,12 @@ class ReportController extends Controller
         $today = Carbon::today();
         $total_report = Delivery::select(DB::raw('SUM(amount) AS total_report'))->where('confirm_status',1)->first();
         $oldest_date = Delivery::select('created_at')->orderBy('created_at','asc')->limit(1)->first();
+        $monthly_report = [];
         for($i=0;Carbon::parse($oldest_date->created_at)->addMonth($i)->format('Y-m-d H:i:s')<=$today->format('Y-m-d H:i:s');$i++){
             $monthly_report[Carbon::parse($oldest_date->created_at)->addMonth($i)->format('Y-m')] = Delivery::select(DB::raw('SUM(amount) AS monthly_report'))
                         ->where('confirm_status',1)
                         ->where('created_at','>=',Carbon::parse($oldest_date->created_at)->addMonth($i)->format('Y-m-01 00:00:00'))
-                        ->where('created_at','<=',Carbon::parse($oldest_date->created_at)->addMonth($i)->lastOfMonth())
+                        ->where('created_at','<=',Carbon::parse($oldest_date->created_at)->addMonth($i)->lastOfMonth()->format('Y-m-d 23:59:59'))
                         ->first();
         }
 
